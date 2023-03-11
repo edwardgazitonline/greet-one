df3c375f60999a206e08bf0e1771452416ca6f34
Edward <75829088+edwardgazitonline@users.noreply.github.com>
2023-03-12 01:23:28 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+hidePicture() {
+throw new Error('Method not implemented.');
+}
   title = 'greet-one';
+  firstNameControl: any;
+  lastNameControl: any;
+pic: any;
+edwarsname: any;
+Image: any;
+hide: any;
+  changeName() {
+    this.firstNameControl.setValue('Initial' + ' EdwardGazitsFirstName');
+    this.lastNameControl.setValue('edwardsLastName');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { FirstComponent } from './first/first.component';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+
   ],
   imports: [
     BrowserModule,
